9809d9f23fba9d12900bcc5fd063952d51c6516f
yama-sitter <dadayamism@gmail.com>
2023-08-18 00:08:01 +0900

    chore: environment building

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['.eslintrc.cs']
+};
---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res, next) => {
+  res.end('Top Page');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
