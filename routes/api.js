824338b38e25a1535dba25bd275c8fcc6d2784d1
yama-sitter <dadayamism@gmail.com>
2023-08-21 16:35:23 +0900

    feat(api): add CORS support

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,27 @@
 import { Router, json } from 'express';
 
+const allowList = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://site.example:3000',
+  'http://site.example:3001',
+];
+
 const router = Router();
+
 router.use(json());
+
+router.use((req, res, next) => {
+  if (req.headers.origin && allowList.includes(req.headers.origin)) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', 'X-Token');
+  }
+
+  next();
+});
 
 router.get('/', ({ query, headers }, res) => {
   res.setHeader('X-Timestamp', Date.now());
