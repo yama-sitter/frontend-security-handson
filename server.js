e70839d0e9c2355be86d7369a7734263eab2bd32
yama-sitter <dadayamism@gmail.com>
2023-08-22 15:59:06 +0900

    feat: Add CSP test page and dependencies

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 import express from 'express';
+import { randomBytes } from 'crypto';
 import { api } from './routes/api.js';
 
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use('/api', api);
 
 app.get('/', (_, res) => {
   res.end('Top Page');
+});
+
+app.get('/csp', (_, res) => {
+  const nonceValue = randomBytes(16).toString('base64');
+  res.header(
+    'Content-Security-Policy',
+    `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+    "object-src 'none';" +
+    "base-uri 'none';" +
+    "require-trusted-types-for 'script';",
+  );
+  res.render('csp', { nonce: nonceValue });
 });
 
 app.listen(port, () => {
