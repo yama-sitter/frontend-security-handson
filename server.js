c963788e52edc0af82034206b460e09c3293e2bb
yama-sitter <dadayamism@gmail.com>
2023-08-22 17:32:42 +0900

    feat: Add CSRF protection to the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { randomBytes } from 'crypto';
 import { api } from './routes/api.js';
+import { csrf } from './routes/csrf.js';
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use('/api', api);
+app.use('/csrf', csrf);
 
 app.get('/', (_, res) => {
   res.end('Top Page');
@@ -19,9 +21,9 @@ app.get('/csp', (_, res) => {
   res.header(
     'Content-Security-Policy',
     `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
-    "object-src 'none';" +
-    "base-uri 'none';" +
-    "require-trusted-types-for 'script';",
+      "object-src 'none';" +
+      "base-uri 'none';" +
+      "require-trusted-types-for 'script';",
   );
   res.render('csp', { nonce: nonceValue });
 });
